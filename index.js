4e3ae6b35c83172b4745f3063d3752661651a5ef
Tetiana Brzezinska <tetianabrzezinska@Tetianas-MacBook-Pro.local>
2024-10-02 20:37:44 +0200

    lesson_1 finished

---
+++ index.js
@@ -0,0 +1,8 @@
+function recursiveIteration(array, index = 0) {   
+    if (index >= array.length) return; 
+    console.log(array[index]); 
+    recursiveIteration(array, index + 1);
+}
+
+const arr = [1, 2, 3, 4, 5];
+recursiveIteration(arr);
